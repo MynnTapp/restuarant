670b43de12cf4ebb812c4f379079e9ac9933e4da
MynnTapp <Desmynnmjc@gmail.com>
2025-01-08 08:31:49 -0600

    fixed index.js file

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -138,16 +138,16 @@ if (process.env.NODE_ENV === "production") {
   // Serve the frontend's index.html file at the root route
   router.get("/", (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(path.resolve(__dirname, "../../frontend/dist", "index.html"));
+    return res.sendFile(path.resolve(__dirname, "../../frontend", "dist", "index.html"));
   });
 
   // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve(__dirname, "../../frontend/dist")));
+  router.use(express.static(path.resolve("../frontend/dist")));
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(path.resolve(__dirname, "../../frontend/dist", "index.html"));
+    return res.sendFile(path.resolve(__dirname, "../../frontend", "dist", "index.html"));
   });
 }
 
