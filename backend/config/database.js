f961d0201e34907b874729003725db0b05cdd2c9
MynnTapp <Desmynnmjc@gmail.com>
2025-01-07 22:50:56 -0600

    added config.dbfile

--- backend/config/database.js
+++ backend/config/database.js
@@ -29,7 +29,7 @@ const config = require("./index");
 
 module.exports = {
   development: {
-    storage: './dev.db',  // Uses the dbFile defined in index.js
+    storage: config.dbFile, // Uses the dbFile defined in index.js
     dialect: "sqlite",
     seederStorage: "sequelize",
     logQueryParameters: true,
