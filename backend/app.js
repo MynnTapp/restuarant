beb9be8901b70c1edebae051befacdf2c69410f8
MynnTapp <Desmynnmjc@gmail.com>
2025-01-08 08:52:53 -0600

    fixed build commands

--- backend/app.js
+++ backend/app.js
@@ -1,93 +1,4 @@
 
-// const express = require("express");
-// require("express-async-errors");
-// const morgan = require("morgan");
-// const cors = require("cors");
-// const csurf = require("csurf");
-// const helmet = require("helmet");
-// const cookieParser = require("cookie-parser");
-// const routes = require("./routes/api");
-// const { environment } = require("./config");
-// const { ValidationError } = require("sequelize");
-// const { restoreUser, refreshToken } = require("./utils/auth");
-
-// const app = express();
-// const isProduction = environment === "production";
-
-// // Logging and Parsing
-// app.use(morgan("dev"));
-// app.use(cookieParser());
-// app.use(express.json());
-
-// // Security Middleware
-// if (!isProduction) {
-//   app.use(cors());
-// }
-// app.use(
-//   helmet.crossOriginResourcePolicy({
-//     policy: "cross-origin",
-//   })
-// );
-
-// // CSRF Middleware (global, applied to all routes)
-// app.use(
-//   csurf({
-//     cookie: {
-//       secure: isProduction,
-//       sameSite: isProduction && "Lax",
-//       httpOnly: true,
-//     },
-//   })
-// );
-
-// // Define the `/api/csrf/restore` route AFTER the csurf middleware
-// app.use("/api/csrf/restore", (req, res) => {
-//   const csrfToken = req.csrfToken();
-//   res.cookie("XSRF-TOKEN", csrfToken);
-//   res.status(200).json({ "XSRF-Token": csrfToken });
-// });
-
-// // Custom Middleware
-// app.use(restoreUser);
-// app.use(refreshToken);
-
-// // Routes
-// app.use("/api", routes);
-
-// // Error Handling
-// app.use((_req, _res, next) => {
-//   const err = new Error("The requested resource couldn't be found.");
-//   err.title = "Resource Not Found";
-//   err.errors = { message: "The requested resource couldn't be found." };
-//   err.status = 404;
-//   next(err);
-// });
-
-// app.use((err, _req, _res, next) => {
-//   if (err instanceof ValidationError) {
-//     const errors = {};
-//     for (const error of err.errors) {
-//       errors[error.path] = error.message;
-//     }
-//     err.title = "Validation error";
-//     err.errors = errors;
-//   }
-//   next(err);
-// });
-
-// app.use((err, _req, res, _next) => {
-//   res.status(err.status || 500);
-//   console.error(err);
-//   res.json({
-//     message: err.message,
-//     errors: err.errors,
-//   });
-// });
-
-// module.exports = app;
-
-
-// IMPORTS
 const express = require("express");
 require("express-async-errors");
 const morgan = require("morgan");
@@ -95,20 +6,19 @@ const cors = require("cors");
 const csurf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
-const app = express();
-const routes = require("./routes");
 const { environment } = require("./config");
-const isProduction = environment === "production";
+const routes = require("./routes");
 const { ValidationError } = require("sequelize");
-const csrfRouter = require("../backend/routes/index");
-const { restoreUser, refreshToken } = require("./utils/auth");
 
+const isProduction = environment === "production";
+const app = express();
+
+// Middleware
 app.use(morgan("dev"));
+
 app.use(cookieParser());
+
 app.use(express.json());
-app.use("/api", csrfRouter);
-app.use(restoreUser);
-app.use(refreshToken);
 
 // Security Middleware
 if (!isProduction) {
@@ -134,13 +44,11 @@ app.use(
   })
 );
 
-app.use(routes);
+app.use(routes); // Connect all the routes
 
 // Catch unhandled requests and forward to error handler.
 app.use((_req, _res, next) => {
   const err = new Error("The requested resource couldn't be found.");
-  err.title = "Resource Not Found";
-  err.errors = { message: "The requested resource couldn't be found." };
   err.status = 404;
   next(err);
 });
@@ -153,22 +61,17 @@ app.use((err, _req, _res, next) => {
     for (let error of err.errors) {
       errors[error.path] = error.message;
     }
-    err.title = "Validation error";
     err.errors = errors;
   }
   next(err);
 });
 
-// RETURN TO COMMENT OUT TITLE AND STACK
 // Error formatter
 app.use((err, _req, res, _next) => {
-  res.status(err.status || 500);
-  console.error(err);
-  res.json({
-    // title: err.title || "Server Error",
+  res.status(err.status || 500).json({
     message: err.message,
     errors: err.errors,
-    // stack: isProduction ? null : err.stack,
+    stack: isProduction ? undefined : err.stack,
   });
 });
 
