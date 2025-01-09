1623ae076cac79cd37725eb55f3f7f3a29a01c10
MynnTapp <Desmynnmjc@gmail.com>
2025-01-09 13:13:36 -0600

    fixed modal

--- frontend/src/context/Modal.jsx
+++ frontend/src/context/Modal.jsx
@@ -43,10 +43,19 @@ export function Modal() {
   if (!modalRef || !modalRef.current || !modalContent) return null;
 
   // Render the following component to the div referenced by the modalRef
+  // return ReactDOM.createPortal(
+  //   <div id="modal">
+  //     <div id="modal-background" onClick={closeModal} />
+  //     <div id="modal-content">{modalContent}</div>
+  //   </div>,
+  //   modalRef.current
+  // );
+
   return ReactDOM.createPortal(
-    <div id="modal">
-      <div id="modal-background" onClick={closeModal} />
-      <div id="modal-content">{modalContent}</div>
+    <div id="modal-overlay" onClick={closeModal}>
+      <div id="modal" onClick={(e) => e.stopPropagation()}>
+        <div id="modal-content">{modalContent}</div>
+      </div>
     </div>,
     modalRef.current
   );
