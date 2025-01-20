3792f504d852f52aa99e58471d970f0ef0f8fece
MynnTapp <Desmynnmjc@gmail.com>
2025-01-20 13:02:08 -0600

    fixing session.js api

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -51,7 +51,7 @@ router.post("/", validateLogin, async (req, res, next) => {
     username: user.username,
   };
 
-  await setTokenCookie(res, safeUser);
+  setTokenCookie(res, safeUser);
 
   return res.json({
     user: safeUser,
